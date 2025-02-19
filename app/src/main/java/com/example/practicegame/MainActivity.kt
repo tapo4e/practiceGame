556ede20e86e619896c7e4da02cb796ea379c062
bylinskiynikiya796@gmail.com <bylinskiynikiya796@gmail.com>
2025-02-20 00:53:29 +0300

    change main

--- app/src/main/java/com/example/practicegame/MainActivity.kt
+++ app/src/main/java/com/example/practicegame/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.practicegame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.practicegame.ui.pages.StartPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         SecondView()
+         StartPage(onClick = {})
             }
         }
     }
